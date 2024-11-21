3bf42ac4b98e8f84ffaea3b25729f5c26b2dbadb
Ram Sharma <ramksharma08@gmail.com>
2024-11-21 10:30:14 -0500

    Commenting on code for better understanding

--- src/App.js
+++ src/App.js
@@ -13,14 +13,23 @@ function App() {
 		setCalc(calc + value);
 	}
 
+	
 	const createDigits = () => {
+		
+		//Initializes an empty array called digits.
 		const digits = [];
 		
+		//Loops from 1 to 9 (inclusive of 1, exclusive of 10).
 		for (let i = 1; i < 10; i++) {
+			/*Adds a new element to the digits array during 
+			each iteration of the loop. The element being added is a 
+			<button> component with a key and the digit as its content.*/
 			digits.push(
 				<button key={i}> {i} </button>
 			)
 		}
+		/*Returns the digits array, which now contains 
+		  all the buttons for the numbers 1 through 9.*/
 		return digits;
 	}
 
